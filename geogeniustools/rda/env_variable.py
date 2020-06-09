320e855c517726adc257a813d483ad080a7fb732
Ashwin Nair <ash1995@gmail.com>
2020-06-09 09:46:50 +0400

    Update env_variable.py

--- geogeniustools/rda/env_variable.py
+++ geogeniustools/rda/env_variable.py
@@ -1,9 +1,9 @@
 import os
 
-USER_ENDPOINT = os.environ.get("USER_ENDPOINT", 'http://100.94.17.149:9000/v1.0')
+USER_ENDPOINT = os.environ.get("USER_ENDPOINT", 'http://114.116.224.168:9003/v1.0')
 
-MANAGER_ENDPOINT = os.environ.get("MANAGER_ENDPOINT", 'http://100.94.17.149:9000/v1.0')
+MANAGER_ENDPOINT = os.environ.get("MANAGER_ENDPOINT", 'http://114.116.224.168:9003/v1.0')
 
-RDA_ENDPOINT = os.environ.get("RDA_ENDPOINT", "http://100.94.17.149:9000/v1.0")
+RDA_ENDPOINT = os.environ.get("RDA_ENDPOINT", "http://114.116.224.168:9003/v1.0")
 
-PUBLIC_RDA_ENDPOINT = os.environ.get("PUBLIC_RDA_ENDPOINT", "http://100.94.17.149:9000/v1.0")
+PUBLIC_RDA_ENDPOINT = os.environ.get("PUBLIC_RDA_ENDPOINT", "http://114.116.224.168:9003/v1.0")
